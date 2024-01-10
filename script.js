c7fc573526d48e355434ba2abb0ea20ef3367959
RizkaAuliarahmi <58288894+RizkaAuliarahmi@users.noreply.github.com>
2024-01-10 20:44:32 +0700

    Update

--- script.js
+++ script.js
@@ -2,34 +2,34 @@ $(document).ready(function () {
     $('nav a').on('click', function (event) {
         event.preventDefault();
         var target = $(this).attr('href');
-        $.scrollify.move(target);
-    });
-
-    $.scrollify({
-        section: ".scroll",
-        sectionName: "section-name",
-        easing: "easeOutExpo",
-        scrollSpeed: 1100,
-        offset: 0,
-        scrollbars: true,
-        setHeights: true,
-        overflowScroll: true,
-        updateHash: false,
-        touchScroll: true,
-    });
-
-    $(window).on('scroll', function () {
-        var scrollPosition = $(window).scrollTop();
-        $('section').each(function () {
-            var offset = $(this).offset().top;
-            console.log("ofset"+offset);
-            var sectionId = $(this).attr('data-section-name');
-            console.log("sectionId"+sectionId);
-
-            if (scrollPosition >= offset && scrollPosition < offset + $(this).height()) {
-                $('nav a').removeClass('active');
-                $('nav a[href="#' + sectionId + '"]').addClass('active');
-            }
+        $.scrollify.move(target, 0, function () {
+            updateActiveNav();
         });
     });
+
+$.scrollify({
+    section: ".scroll",
+    sectionName: "section-name",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset: 0,
+    scrollbars: true,
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: false,
+    touchScroll: true,
+});
+
+$(window).on('scroll', function () {
+    var scrollPosition = $(window).scrollTop();
+    $('section').each(function () {
+        var offset = $(this).offset().top;
+        var sectionId = $(this).attr('data-section-name');
+
+        if (scrollPosition + 20 >= offset && scrollPosition < offset + $(this).height()) {
+            $('nav a').removeClass('active');
+            $('nav a[href="#' + sectionId + '"]').addClass('active');
+        }
+    });
+});
 });
